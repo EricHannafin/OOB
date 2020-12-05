1de32bc55f3610dcae16db79da4e815367f7cd36
Eric Hannafin <eric.hannafin@students.ittralee.ie>
2020-12-05 12:13:25 +0000

    05/11/20

---
+++ Practice/enchancedForLoop.java
@@ -0,0 +1,12 @@
+public class enchancedForLoop {
+
+    public static void main(String[] args) {
+
+        String shop[] = {"Eggs", "Milk", "Bread"};
+
+        // used to print array
+        for (String item : shop){
+            System.out.println(item);
+        }
+    }
+}
